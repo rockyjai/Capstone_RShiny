9245128073305b46728333166a8853e8e3a9dfa4
RCollins113 <83550095+RCollins113@users.noreply.github.com>
2021-07-21 08:18:18 -0400

    Add files via upload

---
+++ ui.R
@@ -0,0 +1,22 @@
+# Load required libraries
+require(leaflet)
+
+
+# Create a RShiny UI
+shinyUI(
+  fluidPage(padding=5,
+  titlePanel("Bike-sharing demand prediction app"), 
+  # Create a side-bar layout
+  sidebarLayout(
+    # Create a main panel to show cities on a leaflet map
+    mainPanel(
+      # leaflet output with id = 'city_bike_map', height = 1000
+      plotOutput("city_bike_map", height = "1000")
+    ),
+    # Create a side bar to show detailed plots for a city
+    sidebarPanel(
+      # select drop down list to select city
+      selectInput(inputId="city_dropdown", "Select City:",
+                  choices = c("All", "Seoul", "Suzhou", "London", "New York", "Paris"))
+    ))
+))
